f02949a7accc731692a53442dfe83071b0e11f6f
Yunosuke Sakai <qmkns246@gmail.com>
2024-11-25 12:24:50 +0900

    first commit

---
+++ SwiftFormatWithGitHubActions/SwiftFormatWithGitHubActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFormatWithGitHubActionsApp.swift
+//  SwiftFormatWithGitHubActions
+//
+//  Created by sakai.yunosuke on 2024/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFormatWithGitHubActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
